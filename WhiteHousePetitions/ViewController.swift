eff488f1fddd07690fa844cd9cd4ee7f62ea8761
rdeshmu <rajivdeshmukh15@gmail.com>
2017-07-27 00:00:16 -0700

    Implemented Easy GCD using peformSelector

--- WhiteHousePetitions/ViewController.swift
+++ WhiteHousePetitions/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+    
+    func fetchJSON() {
         let urlString: String
         
         if navigationController?.tabBarItem.tag == 0 {
@@ -23,19 +27,17 @@ class ViewController: UITableViewController {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
-        DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
-            if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url) {
-                    let json = JSON(data: data)
-                    
-                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-                        self.parse(json: json)
-                        return
-                    }
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    self.parse(json: json)
+                    return
                 }
             }
-        }        
-        showError()
+        }
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
     }
     
     func parse(json: JSON) {
@@ -47,9 +49,7 @@ class ViewController: UITableViewController {
             petitions.append(obj)
         }
         
-        DispatchQueue.main.async { [unowned self] in
-            self.tableView.reloadData()
-        }
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
     }
 
     func showError() {
